d151a371247f6c930f1106061e549401528703d3
GABRIEL DIAZ <gabriel.diazl@usm.cl>
2024-04-30 17:32:22 +0200

    Completed news Finder

--- scraping/newsFinder.py
+++ scraping/newsFinder.py
@@ -3,40 +3,75 @@
 import requests
 import nltk
 from newspaper import Article
+from newspaper.article import ArticleException
 
-def newsFinder(keywords, api_key):
+def newsFinder(keywords, api_key): #This function returns an array with [[Title1, Country1, Url1],[Title2, Country2, Url2],...]
     url = f"https://newsdata.io/api/1/news?apikey={api_key}&q={keywords}"
     response = requests.get(url)
     data = response.json()
-    urls = []
+    newsData = []
 
     if "results" in data:
         articles = data["results"]
         for article in articles:
+            newdat = []
             title = article.get("title", "Title not available")
-            countries = article.get("country", ["Country not available"])
+            country = article.get("country", ["Country not available"])
             url = article.get("link", "URL not available")
-            urls.append(url)
+
+            if isinstance(title, list) and len(title) == 1:
+                title = title[0]
+            if isinstance(country, list) and len(country) == 1:
+                country = country[0]
+            if isinstance(url, list) and len(url) == 1:
+                url = url[0]
+
+            newdat.append(title)
+            newdat.append(country)
+            newdat.append(url)
+            newsData.append(newdat)
     
-    return(urls)
+    return(newsData)
 
 def newsExtractContent(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    nltk.download('punkt')
-    article.nlp()
+    data = []
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
 
-    #print(article.authors)
-    #print(article.publish_date)
-    #print(article.top_image)
-    print(article.text)
+        date = article.publish_date
+        body = article.text
+        summary = article.summary
 
-keywords = "Palestine Conflict" #Input by the user
+        data.append(date)
+        data.append(body)
+        data.append(summary)
+    except ArticleException as e:
+        print(f"Error downloading article from {url}: {e}")
+        data = None
+    return(data)
+
+nltk.download('punkt')
+
+keywords = "Labor Day" #Input by the user
 api_key = 'pub_43149e792f981a89e8244c3d6ec8030fae0da'
-urls = newsFinder(keywords, api_key) #Arrays with all the URLs collected
+newData = newsFinder(keywords, api_key) #Arrays with all the URLs collected
 
-#for url in urls:
+""" dat = newsExtractContent(newData[2][2])
+print(dat) """
+
+completeData = []
+for newsItem in newData:
+    title, country, url = newsItem
+    contentData = newsExtractContent(url)
+    if contentData is not None:
+        completeNewsItem = [title, country, url] + contentData
+        completeData.append(completeNewsItem)
+
+print(len(completeData))
+
 
 
 
