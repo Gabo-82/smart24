0acf318e0da75fa1d9acb941d2226c289d7dbf53
misaki <mxm1407@case.edu>
2024-05-03 14:45:54 +0200

    added sentiment bubble component (not shown on page yet)

---
+++ smart_app/src/app/sentiment-bubble/sentiment-bubble.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sentiment-bubble',
+  standalone: true,
+  imports: [],
+  templateUrl: './sentiment-bubble.component.html',
+  styleUrl: './sentiment-bubble.component.css'
+})
+export class SentimentBubbleComponent {
+
+}
