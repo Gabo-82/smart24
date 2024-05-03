c23880a7ea18f3d7ca41ce5adc9947f134660b69
elvilehtonen <peppilotta.lehtonen@gmail.com>
2024-05-03 10:56:37 +0200

    The subtopics work

--- smart_app/src/app/event-card/event-card.component.ts
+++ smart_app/src/app/event-card/event-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject} from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'; 
 import { Router } from '@angular/router';
 
 
@@ -9,14 +9,29 @@ import { Router } from '@angular/router';
   styleUrl: './event-card.component.css'
 })
 export class EventCardComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private router: Router) { }
+  // constructor(@Inject(MAT_DIALOG_DATA) public data: any, private router: Router) { }
+  // constructor(private dialogRef: MatDialogRef<YourDialogComponent>, private router: Router) {}
+  constructor(
+    private dialogRef: MatDialogRef<EventCardComponent>, // Corrected component name here
+    private router: Router,
+    @Inject(MAT_DIALOG_DATA) public data: any // Inject MAT_DIALOG_DATA to access data passed to the dialog
+  ) {}
+
   redirect():void{
     this.router.navigate(["details"]);
   }
 
-  onClickSubtopic(subtopic: string): void {
+  onClickSubtopic_first_version(subtopic: string): void {
     // Implement logic based on the clicked subtopic (e.g., navigate, show content, etc.)
     console.log(`Clicked on subtopic: ${subtopic}`);
     // Add more logic as needed based on the clicked subtopic
   }
-}
+
+  onClickSubtopic(subtopic: string): void {
+    if (subtopic.trim() !== '') {
+      this.router.navigate(['/worldmap', subtopic.trim()]).then(() => {
+        this.dialogRef.close();
+      });
+    }
+  }
+}  
