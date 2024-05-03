ca1fd5bd5da4eafb6297aea39969e083c5b4773e
진의영 <jineuiyoung@jin-uiyeong-ui-MacBookPro.local>
2024-05-03 09:11:28 +0200

    slight change

--- smart_app/src/app/sentiment_analysis.py
+++ smart_app/src/app/sentiment_analysis.py
@@ -9,10 +9,12 @@ def get_sentiment(text):
         model="gpt-3.5-turbo",
         messages=[{"role": "system", "content": f"Sentiment analysis of the following text: answering with 0 to 10\n{text}\n"}]
                   )
-    return response
+    sentiment = response["choices"][0]["message"]["content"]
+    return str(sentiment) 
 
 
-db_file = '/Users/jineuiyoung/Downloads/news_articles.sqlite'
+db_file = '/Users/jineuiyoung/Downloads/news_articles2_.sqlite'
+new_db_file = '/Users/jineuiyoung/Downloads/news_articles2_ai.sqlite'
 
 
 conn = sqlite3.connect(db_file)
@@ -20,8 +22,40 @@ query = "SELECT title, country, body FROM Articles"
 df = pd.read_sql_query(query, conn)
 conn.close()
 
+new_conn = sqlite3.connect(new_db_file)
+new_cursor = new_conn.cursor()
+
+
+# try:
+#     cursor.execute("ALTER TABLE Articles ADD COLUMN sentiment TEXT")
+# except sqlite3.OperationalError:
+#     pass
+
+new_cursor.execute("""CREATE TABLE IF NOT EXISTS Articles (
+                    id INTEGER PRIMARY KEY,
+                    title TEXT,
+                    country TEXT,
+                    body TEXT,
+                    sentiment TEXT)""")
+
 for index, row in df.iterrows():
-    sentiment = get_sentiment(row['body'])["choices"][0]["message"]["content"]
+    sentiment = get_sentiment(row['body'])
+    title = row['title']
+    country = row['country']
+    body = row['body']
+    
+    # print(sentiment)
+    new_cursor.execute("INSERT INTO Articles (title, country, body, sentiment) VALUES (?, ?, ?, ?)",
+                   (title, country, body, sentiment))
+    new_conn.commit()
+
+new_conn.close()
+
+print("godd")
+
+
+for index, row in df.iterrows():
+    sentiment = get_sentiment(row['body'])
     print(f"<{row['title']}> from {row['country']} has {sentiment} sentimental score\n")
     
 
