76815931bebb18167b071e39fb22a318a3e32322
GABRIEL DIAZ <gabriel.diazl@usm.cl>
2024-05-04 20:39:07 +0200

    I hate Github

--- python_app/get_sentiment.py
+++ python_app/get_sentiment.py
@@ -1,7 +1,7 @@
 import openai
 
-openai.api_key = "API KEY"
-
+text = "IM happy"
+openai.api_key = "DONT USE THIS KEY, USE YOUR OWN"
 
 def get_sentiment(text):
     response = openai.chat.completions.create(
@@ -10,10 +10,10 @@ def get_sentiment(text):
                    Secondly, analyse which of these sentiments would describe the sentiment of the text best, this answer must be one word from this sentiment list "hopeful, celebratory, informative, critical, angry, sad".
                    Thirdly, tell me if it's good news or bad news for a general citizen. You must answer with only "good" or "bad". 
                    Lastly, tell me if this article seems biased or neutral. Also in this case you must answer with one of those two words.
-                   Please seperate your answer with comas. For example, '5, inspiring, good, neutral'. In case no text is provided, just answer 
-                   -, -, -, -\n{text}\n"""}]
+                   Please seperate your answer with comas. For example, '5, inspiring, good, neutral
+                   If you cant access to the text, just answer -, -, -, -\n{text}\n"""}]
                   )
-    
     sentiment = response.choices[0].message.content
-    score, sentiment, goodOrbad, bias = sentiment.split(", ")
-    return score, sentiment, goodOrbad, bias
+    score, sentiment, goodOrbad, bias = sentiment.split(",") 
+    # print(sentiment)
+    return (score, sentiment, goodOrbad, bias)
