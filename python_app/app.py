6b56945f4ebb0baf3d6b99fe3b96763547fbf161
GABRIEL DIAZ <gabriel.diazl@usm.cl>
2024-05-05 12:32:29 +0200

    Changed the search__articles_by_keyword function, now it uses the load_short_articles_to_db function

--- python_app/app.py
+++ python_app/app.py
@@ -21,13 +21,6 @@ if (short_data[0] != ""):
 load_full_body_to_db()
 load_sentiment_to_db()
 
-""" keywords = "USA RIOTS"
-short_data = newsFinder(keywords, api_key)
-if (short_data[0] != ""):
-    load_short_articles_to_db(short_data, keywords)
-
-load_full_body_to_db()
-load_sentiment_to_db() """
 
 
 # Routing for API endpoints
@@ -99,38 +92,14 @@ def get_articles_by_keyword(keyword):
 
 @app.route('/api/searchOnline/<keyword>') # This one is for the worldmap
 def search_articles_by_keyword(keyword):
-    conn = sqlite3.connect(SQL_FILE)
-    cursor = conn.cursor()
-
-    api_key = 'pub_43149e792f981a89e8244c3d6ec8030fae0da'
+    api_key = newsapi_key
     short_data = newsFinder(keyword, api_key)
 
-    for newsItem in short_data:
-        print(newsItem)
-        # 485 is last proper row
-        title, country, url, key_words, date, img_url, category, description, language = newsItem
-        # THIS FAILS sometimes because country is a list sometimes, news_finder has to be edited
-        try:
-            cursor.execute("""INSERT INTO Keywords (Keyword) VALUES (?)""", (keyword,))
-        except sqlite3.Error as er:
-            print(er.sqlite_errorcode)
-            print(er.sqlite_errorname)
-        try:
-            cursor.execute("""INSERT INTO Articles (title, country, url, keyWords, imgUrl, date,
-                                                                        category, description, language)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-                    (title, str(country), url, str(key_words), date, img_url, category, description, language))
-        except sqlite3.ProgrammingError as er:
-            print(er.sqlite_errorcode)
-            print(er.sqlite_errorname)
-            for item in newsItem:
-                print(item)
-        cursor.execute("""INSERT INTO ArticleKeywords (id, KeywordID)
-        SELECT last_insert_rowid(), k.KeywordID
-        FROM Keywords as k
-        WHERE k.Keyword = ?;
-        """, (keyword,))
-    conn.commit()
+    if (short_data[0] != ""):
+        load_short_articles_to_db(short_data, keywords) #This is the function that loads the articles to the database
+
+    conn = sqlite3.connect(SQL_FILE)
+    cursor = conn.cursor()
 
     sql_query = """SELECT a.*
     FROM Articles as a
